157036261fa425f2db4efc01b2ca9bfe3e7400c2
fai-songprasit <prapawan.songprasit@gmail.com>
2020-07-06 15:05:19 +1200

    crete route, link to server and test with postman (step 4)

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const todos = require('./routes/todos')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static('public'))
+
+server.use('/api/v1/todos', todos)
 
 module.exports = server
